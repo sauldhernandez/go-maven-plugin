114f43a94ddfd7993eb504aedcab83028fd61022
Saul Hernandez <saul.hernandez@synergy-gb.com>
2014-10-03 23:29:01 -0430

    Basic functionality achieved.
    Can now build by specifying goals. Properties still not taken into account.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-go-maven-plugin
+Go Maven Plugin
 ===============
+
+A Maven task runner for Go (http://www.go.cd).
+ 
+
--- src/main/java/org/sauldhernandez/MavenTaskPlugin.java
+++ src/main/java/org/sauldhernandez/MavenTaskPlugin.java
@@ -1,27 +1,35 @@
 package org.sauldhernandez;
 
 import com.thoughtworks.go.plugin.api.annotation.Extension;
-import com.thoughtworks.go.plugin.api.config.Option;
-import com.thoughtworks.go.plugin.api.config.Property;
 import com.thoughtworks.go.plugin.api.response.execution.ExecutionResult;
 import com.thoughtworks.go.plugin.api.response.validation.ValidationResult;
 import com.thoughtworks.go.plugin.api.task.*;
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.shared.invoker.*;
+
+import java.io.*;
+import java.util.Arrays;
 
 /**
- * Hello world!
+ * Provides the implementation of the Task extension point, and executes
+ * the necessary logic for the operation of the plugin.
  *
+ * @author Saúl Hernández
+ * @since 1.0
  */
 @Extension
 public class MavenTaskPlugin implements Task
 {
     private static final String GOAL_KEY = "goal";
     private static final String PROPERTIES_KEY = "properties";
+    private static final String WORKDIR_KEY = "workdir";
 
     @Override
     public TaskConfig config() {
         TaskConfig config = new TaskConfig();
         config.addProperty(GOAL_KEY);
         config.addProperty(PROPERTIES_KEY);
+        config.addProperty(WORKDIR_KEY).withDefault(".");
 
         return config;
     }
@@ -30,8 +38,49 @@ public class MavenTaskPlugin implements Task
     public TaskExecutor executor() {
         return new TaskExecutor() {
             @Override
-            public ExecutionResult execute(TaskConfig taskConfig, TaskExecutionContext taskExecutionContext) {
-                return ExecutionResult.success("Done nothing...");
+            public ExecutionResult execute(TaskConfig taskConfig, TaskExecutionContext context) {
+
+                try {
+                    PipedInputStream inOut = new PipedInputStream();
+                    PipedInputStream inErr = new PipedInputStream();
+
+                    PrintStream errStream = new PrintStream(new PipedOutputStream(inOut));
+                    PrintStream outStream = new PrintStream(new PipedOutputStream(inErr));
+                    context.console().readErrorOf(inErr);
+                    context.console().readOutputOf(inOut);
+
+                    PrintStream oldOut = System.out;
+                    PrintStream oldErr = System.err;
+
+                    System.setOut(outStream);
+                    System.setErr(errStream);
+
+                    InvocationRequest request = new DefaultInvocationRequest();
+                    request.setGoals(Arrays.asList(taskConfig.getValue(GOAL_KEY).split(" ")));
+                    request.setBaseDirectory(new File(context.workingDir(), taskConfig.getValue(WORKDIR_KEY)));
+
+                    Invoker invoker = new DefaultInvoker();
+                    invoker.setMavenHome()
+
+                    try {
+                        InvocationResult result = invoker.execute(request);
+                        if(result.getExitCode() == 0)
+                            return ExecutionResult.success("Maven execution completed successfully.");
+                        else
+                            return ExecutionResult.failure("Maven execution failed.");
+                    }
+                    catch(MavenInvocationException e) {
+                        return ExecutionResult.failure("Failed to execute maven.", e);
+                    }
+                    finally {
+                        System.setOut(oldOut);
+                        System.setErr(oldErr);
+                    }
+
+                }
+                catch(IOException e) {
+                    return ExecutionResult.failure("Failed to pipe output", e);
+                }
             }
         };
     }
@@ -46,12 +95,11 @@ public class MavenTaskPlugin implements Task
 
             @Override
             public String template() {
-                return  "<div class=\"form_item_block\">" +
-                        "   <label>Goal:<span class=\"asterisk\">*</span>" +
-                        "       <input type=\"text\" ng-model\"" + GOAL_KEY + "\" ng-required=\"true\"/>"+
-                        "   </label>" +
-                        "</div>";
-
+                try {
+                    return IOUtils.toString(getClass().getResourceAsStream("/views/task.template.html"), "UTF-8");
+                } catch (Exception e) {
+                    return "Failed to find template: " + e.getMessage();
+                }
             }
         };
     }
